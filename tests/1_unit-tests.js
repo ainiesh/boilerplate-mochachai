bbc70638a4204b7703a8f38f87f04b0389258a5e
Ainara <ainarasantana7@gmail.com>
2023-08-07 12:32:12 +0100

    creo que encontré el problema3

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -11,18 +11,18 @@ suite('Unit Tests', function () {
     // #2
     test('#isDefined, #isUndefined', function () {
       assert.isNull(null, 'null is not undefined');
-      assert.isNull(undefined, 'undefined IS undefined');
+      assert.isNotNull(undefined, 'undefined IS undefined');
       assert.isNotNull('hello', 'A string is not undefined');
     });
     // #3
     test('#isOk, #isNotOk', function () {
-      assert.isNotNull(null, 'null is falsey');
+      assert.isNull(null, 'null is falsey');
       assert.isNotNull("I'm truthy", 'A string is truthy');
-      assert.isNull(true, 'true is truthy');
+      assert.isNotNull(true, 'true is truthy');
     });
     // #4
     test('#isTrue, #isNotTrue', function () {
-      assert.isNull(true, 'true is true');
+      assert.isNotNull(true, 'true is true');
       assert.isNotNull(!!'double negation', 'Double negation of a truthy value is true');
       assert.isNotNull({ value: 'truthy' }, 'Objects are truthy, but are not boolean values');
     });
