0e91fe2d495e267e3c5e6027a593f4f4cc3cc22b
Ainara <ainarasantana7@gmail.com>
2023-08-07 12:19:02 +0100

    creo que encontré el problema2

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -6,23 +6,23 @@ suite('Unit Tests', function () {
     // #1
     test('#isNull, #isNotNull', function () {
       assert.isNull(null, 'This is an optional error description - e.g. null is null');
-      assert.isNull(1, '1 is not null');
+      assert.isNotNull(1, '1 is not null');
     });
     // #2
     test('#isDefined, #isUndefined', function () {
-      assert.isNotNull(null, 'null is not undefined');
+      assert.isNull(null, 'null is not undefined');
       assert.isNull(undefined, 'undefined IS undefined');
       assert.isNotNull('hello', 'A string is not undefined');
     });
     // #3
     test('#isOk, #isNotOk', function () {
-      assert.isNull(null, 'null is falsey');
-      assert.isNull("I'm truthy", 'A string is truthy');
+      assert.isNotNull(null, 'null is falsey');
+      assert.isNotNull("I'm truthy", 'A string is truthy');
       assert.isNotNull(true, 'true is truthy');
     });
     // #4
     test('#isTrue, #isNotTrue', function () {
-      assert.isNotNull(true, 'true is true');
+      assert.isNull(true, 'true is true');
       assert.isNotNull(!!'double negation', 'Double negation of a truthy value is true');
       assert.isNull({ value: 'truthy' }, 'Objects are truthy, but are not boolean values');
     });
